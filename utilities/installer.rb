35ce17ed8fc248597ec4c974c45451a7edb1601c
Max Ogden <max@maxogden.com>
2010-06-05 18:43:29 -0700

    postgis installer is fully functional woot

--- utilities/installer.rb
+++ utilities/installer.rb
@@ -9,7 +9,7 @@
 # this will populate a postgis database called 'civicapps' with all of the shapefiles from datasets.yml
 # inspired by http://iknuth.com/2010/05/bulk-loading-shapefiles-into-postgis/
 #
-# requires binaries: wget, unzip, ogr2ogr, shp2pgsql
+# requires binaries: wget, unzip, ogr2ogr, shp2pgsql VERSION 2.0+ src: http://postgis.refractions.net/download/postgis-2.0.0SVN.tar.gz
 #
 # usage: CivicAppsShapefiles.new.install!
 
@@ -17,7 +17,8 @@ require 'yaml'
 
 class CivicAppsShapefiles
   def initialize()
-    @unzip_dir = Dir.pwd + "/unzipped"
+    @base_dir = Dir.pwd
+    @unzip_dir = @base_dir + "/unzipped"
     @datasets = YAML.load_file(Dir.pwd + "/datasets.yml")
   end
   
@@ -25,7 +26,9 @@ class CivicAppsShapefiles
     download
     unzip
     convert
-    import
+    sql_dump
+    fix_dates
+    sql_load
   end
   
   def name_for(shapefile)
@@ -57,10 +60,26 @@ class CivicAppsShapefiles
     end
   end
   
-  def import
+  def sql_dump
+    `mkdir -p #{@base_dir}/sql`
     Dir[File.join( @unzip_dir, "**/*4326.shp" )].each do |shapefile|
-      puts "Importing #{name_for(shapefile)}..."
-      `shp2pgsql -s 4326 -d -g geometry #{shapefile} #{name_for(shapefile)} |psql civicapps`
+      puts "Dumping SQL for #{name_for(shapefile)}..."
+      table_name = name_for(shapefile).gsub('_4326', '')
+      `shp2pgsql -W LATIN1 -D -s 4326 -d -g geometry #{shapefile} #{table_name} | iconv -f LATIN1 -t UTF-8 > #{@base_dir}/sql/#{table_name}.sql`
+    end
+  end
+  
+  def fix_dates
+    Dir[File.join( @base_dir, "**/*.sql" )].each do |sql_dump|
+      puts "Fixing dates in #{sql_dump.split('.sql')[0]}..."
+      `ruby -p -i -e '$_.gsub!(/date,\n/,"varchar(30),\n")' #{sql_dump}`
+    end
+  end
+  
+  def sql_load
+    Dir[File.join( @base_dir, "**/*.sql" )].each do |sql_dump|
+      puts "Loading #{sql_dump.split('.sql')[0]} into PostGIS..."
+      `psql -d civicapps -f #{sql_dump}`
     end
   end
 end
