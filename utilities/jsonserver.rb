44e0af70bd405999bb5b01f8a77ee360ab87b2ab
Max Ogden <max@maxogden.com>
2010-07-22 11:56:42 -0700

    adding metadata lookup route

--- utilities/jsonserver.rb
+++ utilities/jsonserver.rb
@@ -1,5 +1,5 @@
 get '/' do
-  File.read(File.join('/home/max/web/pdxapi/public', 'index.html'))
+  File.read(File.join('html/', 'index.html'))
 end
 
 get "/:resource" do
@@ -7,7 +7,31 @@ get "/:resource" do
   return {"error" => "You need to pass in an OpenSearch formatted bbox parameter"}.to_json unless params[:bbox]
   results = HTTParty.get("http://localhost:5984/#{params[:resource]}/_design/geojson/_spatial/points?bbox=#{params[:bbox]}")
   results = JSON.parse(results)
-  results = results["spatial"].map{|i| i["value"]}
+  results = results["rows"].map{|i| i["value"]["geometry"].merge({:id => i["value"]["id"]})}
   results = PDXAPI::GeoFunctions.feature_collection_for results
   results.to_json
+end
+
+get "/:resource/:id" do
+  content_type :json
+  results = HTTParty.get("http://localhost:5984/#{params[:resource]}/#{params[:id]}")
+  results = JSON.parse(results)
+  results.to_json
+end
+
+get "/image/:database/:couch_id" do
+<<"EOF"
+  <html>
+  <head>
+  	<meta name="viewport" content="width=320"/>  
+  </head>
+  <body>
+  	<div>
+  		<center>
+  			<img src="http://localhost:5984/#{params[:database]}/#{params[:couch_id]}/menuphoto"/>
+  		</center>
+  	</div>
+  </body>
+  </html>
+EOF
 end
